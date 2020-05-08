20950d6d4a5d6bb0d75adc882f9b021aa29a75e0
auo <andreas.lindgren@outlook.com>
2020-05-08 10:38:49 +0200

    fix: test for upload

---
+++ test/upload.spec.js
@@ -0,0 +1,16 @@
+const upload = require('../src/upload.js');
+const nock = require('nock');
+const assert = require('assert');
+const { Readable } = require('stream');
+
+describe('upload', () => {
+    it('should upload a file to the correct url', async () => {
+        const scope = nock('http://127.0.0.1')
+            .put('/maven-snapshots/com/auo/github/artifact.zip')
+            .reply(200, 'path matched')
+
+        await upload('http://127.0.0.1','maven-snapshots','com/auo/github/artifact.zip',  Readable.from('a'), { username: 'user', password: 'password'});
+
+        assert(scope.isDone(), true);
+    });
+});
