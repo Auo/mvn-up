3d47d65c5d355f2bbe62f6fc831c16e2a9c0d09d
auo <1984096+Auo@users.noreply.github.com>
2022-05-26 21:53:05 +0200

    fix: remove got

--- src/upload.js
+++ src/upload.js
@@ -1,4 +1,5 @@
-const got = require('got');
+const http = require('http');
+const https = require('https');
 
 /**
  * Upload a stream to the specified repository
@@ -8,11 +9,52 @@ const got = require('got');
  * @param stream stream to upload
  * @param auth { username: username, password: password }
  */
-module.exports = async (url, path, stream, auth) => {
-    const fullUrl = `${url}/${path}`;
-    try {
-        await got.put(fullUrl, { username: auth.username, password: auth.password, body: stream });
-    } catch (error) {
-        throw { code: error.response.statusCode, message: error.response.statusMessage, url: fullUrl };
-    }
+module.exports = (url, path, stream, auth) => {
+    return new Promise((resolve, reject) => {
+        const fullUrl = new URL(`${url}/${path}`);
+        const module = fullUrl.protocol === 'https:' ? https : http;
+    
+        let options = {
+            hostname: fullUrl.hostname,
+            port: fullUrl.port,
+            path: fullUrl.pathname + fullUrl.search,
+            method: 'PUT',
+            headers: {}
+        }
+
+        if (!!auth.username && !!auth.password) {
+            options.headers['Authorization'] = 'Basic ' + Buffer.from(`${auth.username}:${auth.password}`).toString('base64');
+        }
+
+        const request = module.request(options, (response) => {
+          const chunks = [];
+
+          response.on('data', (chunk) => {
+            chunks.push(chunk);
+          });
+
+          response.on('end', () => {
+            const body = Buffer.concat(chunks);
+            if (response.statusCode / 100 !== 2) {
+                return reject({
+                    code: response.statusCode,
+                    message: response.statusMessage,
+                    body: body.toString('utf-8'),
+                    url: fullUrl.href
+                })
+            }
+            return resolve();
+          });
+        });
+
+        stream.pipe(request);
+        
+        request.on('error', err => {
+            return reject({code: null, message: err, body: null, url: fullUrl.href });
+        });
+        
+        stream.on('end', () => {
+            request.end();
+        });
+    });
 };
--- test/maven-upload.spec.js
+++ test/maven-upload.spec.js
@@ -38,8 +38,59 @@ describe('file uploading', () => {
             'Distribution',
             path.join(__dirname,'test.zip'),
             'http://127.0.0.1',
-            { username: 'username', password:'password'});
-            assert(scope.isDone(), true);
+            { username: 'username', password: 'password' });
+            assert.ok(scope.isDone());
+    });
+
+    it('should have proper server error message', async () => {
+        const scope = nock('http://127.0.0.1')
+        .put(/.*/)
+        .times(1)
+        .reply(403,  function (_uri, _requestBody) {
+            this.req.response.statusMessage = 'Forbidden';
+            return {
+                status:403,
+                message: 'Not allowed'
+            }
+        });
+
+        try {
+          await mavenUploader(
+            'com.some.group',
+            'some_artifact_id',
+            '1.0.0-SNAPSHOT',
+            'zip',
+            'Distribution',
+            path.join(__dirname, 'test.zip'),
+            'http://127.0.0.1',
+            { username: 'username', password: 'password' }
+          );
+        } catch (err) {
+            assert.equal(err.code, 403);
+            assert.equal(err.message, 'Forbidden');
+            assert.equal(err.url, 'http://127.0.0.1/com/some/group/some_artifact_id/1.0.0-SNAPSHOT/some_artifact_id-1.0.0-SNAPSHOT-Distribution.zip');
+        }
+
+        assert.ok(scope.isDone());
+    });
+
+    it('should have proper client error message', async () => {
+        try {
+            await mavenUploader(
+              'com.some.group',
+              'some_artifact_id',
+              '1.0.0-SNAPSHOT',
+              'zip',
+              'Distribution',
+              path.join(__dirname, 'test.zip'),
+              'http://127.0.0.11',
+              { username: 'username', password: 'password' }
+            );
+          } catch (err) {
+              assert.equal(err.code, null);
+              assert.equal(err.url, 'http://127.0.0.11/com/some/group/some_artifact_id/1.0.0-SNAPSHOT/some_artifact_id-1.0.0-SNAPSHOT-Distribution.zip')
+              assert.ok(err.message);
+          }
     });
 
     afterEach(() => nock.cleanAll());
